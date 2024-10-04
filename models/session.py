8511877a05ca09cb2fa613d9af6ec939c8e79f51
caplerton <caplerton@gmail.com>
2024-10-04 23:44:48 +0200

    added start time und endtime

--- models/session.py
+++ models/session.py
@@ -6,5 +6,7 @@ class WorkshopSession(models.Model):
 
     name = fields.Char(string="Expert Name", required=True)
     description = fields.Text(string="Description")
+    start_time = fields.Datetime(string="Start Time", required=True)
+    end_time = fields.Datetime(string="End Time", required=True)
     expert_id = fields.Many2one('workshop.expert', string="Expert", required=True)
     workshop_ids = fields.Many2one('workshop.workshop', string="Workshops", required=True)
