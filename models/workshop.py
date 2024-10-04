384507e57bbf64fc773c6d1c86f6d434e1772566
caplerton <caplerton@gmail.com>
2024-10-04 19:05:40 +0200

    inital commit

--- models/workshop.py
+++ models/workshop.py
@@ -2,6 +2,22 @@ from odoo import fields, models
 
 class Workshop(models.Model):
     _name = "workshop.workshop"
+    _description = "Workshop element"
+    
     
     name = fields.Char(string="Name", required=True)
     description = fields.Text(string="Description")
+    
+    image = fields.Image(string="Workshop Image")
+    # file = fields.Binary(string="Attached File")
+    # file_name = fields.Char(string="File Name")
+    
+    # file = fields.Binary(string='Attachenment' )
+    attachment_ids = fields.Many2many(
+        comodel_name="ir.attachment",
+        string="Attachments",
+        relation="workshop_attachment_rel",
+        column1="workshop_id",
+        column2="attachment_id",
+        domain="[('res_model', '=', 'workshop.workshop')]"
+    )
