96f78366f95af1ee7e8c29c766d83afa6e2deb94
caplerton <caplerton@gmail.com>
2024-10-08 22:38:34 +0200

    feedback submit

--- models/feeback.py
+++ models/feeback.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class WorkshopFeeback(models.Model):
     _name = 'workshop.session.feedback'
     
-    session_id = fields.Many2one('workshop_session', string="Session", required=True)
+    session_id = fields.Many2one('workshop.session', string="Session", required=True)
     # rating = fields.Selection([(str(i), str(i)) for i in range(1, 6)], string='Bewertung', required=True)
     feedback_text = fields.Text('Feedback')
     
